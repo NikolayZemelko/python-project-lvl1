caab220da63a678033ef4e2d526d26ea4f51e8f1
NikolayZemelko <zemelko1989@gmail.com>
2022-04-26 21:24:52 +0300

    flake8 correcting

--- brain_games/scripts/games/brain_gcd.py
+++ brain_games/scripts/games/brain_gcd.py
@@ -6,12 +6,12 @@ from brain_games.scripts.brain_games import greet
 
 
 def what_is_gcd():
-    
+
     name = greet()
     counter = 3
-    
-    print(f'Find the greatest common divisor of given numbers.')
-    
+
+    print('Find the greatest common divisor of given numbers.')
+
     while counter > 0:
 
         first_num = random.randint(0, 200)
@@ -25,17 +25,17 @@ def what_is_gcd():
                 common_max_div = divider
             divider += 1
 
-    
         print(f'Question: {first_num} {second_num}')
-        answer = prompt.string(f'Your answer: ')
+        answer = prompt.string('Your answer: ')
         correct = str(common_max_div) == answer
 
         if correct:
-            print(f'Correct!')
+            print('Correct!')
         else:
-            print(f'\'{answer}\' is wrong answer ;(. Correct answer \'{common_max_div}\'')
+            print(f'\'{answer}\' is wrong answer ;(.')
+            print(f'Correct answer \'{common_max_div}\'')
             print(f'Let`s try again, {name}')
-            return #EXIT is here, when you are wrong :(
+            return  # EXIT is here, when you are wrong :(
 
         counter -= 1
 
