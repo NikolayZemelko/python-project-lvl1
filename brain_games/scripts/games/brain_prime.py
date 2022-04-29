174c65c76b02c145d3e7d6b21ae66d8abb3be8f6
NikolayZemelko <zemelko1989@gmail.com>
2022-04-29 16:01:22 +0300

    Create new game

---
+++ brain_games/scripts/games/brain_prime.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from sympy import isprime
+import prompt
+import random
+from brain_games.scripts.brain_games import greet
+
+
+def prime_number():
+
+    name = greet()
+    print("Answer \'yes\' if given number is prime. Otherwise answer \'no\'")
+    counter = 3
+
+    while counter > 0:
+
+        num = random.randint(0, 1000)
+        print(f'Question: {num}')
+        answer = prompt.string("Your answer: ")
+        fact = "yes" if isprime(num) else "no"
+
+        is_correct = fact == answer
+
+        if not is_correct:
+
+            print(f'\'{answer}\' is wrong answer ;(.')
+            print(f'Correct answer was \'{fact}\'')
+            print(f'Let`s try again, {name}!')
+            return  # Just leave this game
+
+        print("Correct!")
+        counter -= 1
+
+    print(f'Congratulations, {name}!')
+
+
+def main():
+    prime_number()
+
+
+if __name__ == "__main__":
+    main()
